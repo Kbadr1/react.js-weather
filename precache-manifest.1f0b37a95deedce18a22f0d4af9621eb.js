919634447b065ad300f01cca9726a7f4cf90ea7d
Kbadr1 <kbadr.dev@gmail.com>
2020-07-12 22:51:16 +0200

    Updates

---
+++ precache-manifest.1f0b37a95deedce18a22f0d4af9621eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d20e17da36a9176a7f8095247c6ae398",
+    "url": "/react.js-weather/index.html"
+  },
+  {
+    "revision": "219094b3745021f4bcc1",
+    "url": "/react.js-weather/static/css/main.6a8fb686.chunk.css"
+  },
+  {
+    "revision": "2e535fe193d27241f49c",
+    "url": "/react.js-weather/static/js/2.89f62dc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react.js-weather/static/js/2.89f62dc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "219094b3745021f4bcc1",
+    "url": "/react.js-weather/static/js/main.a3cfb4e4.chunk.js"
+  },
+  {
+    "revision": "7f72baab3b7c1c61bf17",
+    "url": "/react.js-weather/static/js/runtime-main.3b0d92e9.js"
+  },
+  {
+    "revision": "d9cb266798c563836126ad9ddbaa06f2",
+    "url": "/react.js-weather/static/media/pexels-photo-66997.d9cb2667.jpeg"
+  }
+]);
